ec0fc219d822a2ac7339e574921af6c7fa40af98
Misaki <nknmsm1992@gmail.com>
2019-12-14 23:01:51 +0900

    .

--- components/Artwork/index.vue
+++ components/Artwork/index.vue
@@ -23,7 +23,6 @@ export default {
   },
   watch: {
     "$route.name": function(_new, _old){
-
       EventBus.$emit("TRANSITION", _new);
     }
   },
@@ -36,5 +35,10 @@ export default {
 <style>
   .artwork{
     position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #EAF2F5;
   }
 </style>
